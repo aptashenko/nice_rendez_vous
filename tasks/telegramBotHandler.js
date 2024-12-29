30b1dd8f42d11c7ffb1fafbea5a75949c276e605
Artem Ptashenko <aptashenko2019@gmail.com>
2024-12-29 05:20:35 +0200

    changes

--- tasks/telegramBotHandler.js
+++ tasks/telegramBotHandler.js
@@ -125,12 +125,12 @@ export async function startTelegramBot() {
 
         } else if (msg.text === texts.keyboard.description_subscription.button) {
             const planSettings = {
-                amount: 10,
+                amount: 1,
                 label: 'standard',
                 products: {
                     productName: ['Допомогти проекту'],
                     productCount: [1],
-                    productPrice: [10]
+                    productPrice: [1]
                 },
                 currency: 'EUR'
             }
